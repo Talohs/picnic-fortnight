6753510f80a4062fd106b3b43ae4571e46e935f1
Talohs <blackholesoul@gmail.com>
2023-01-09 17:23:31 -0500

    added jinja inheritence

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from app import app
+from flask import render_template
+
+@app.route("/")
+def index():
+    fruits = ['apple', 'banana', 'orange', 'strawberry', 'watermelon', 'mango', 'blueberry']
+    return render_template('index.html', name='Dan', fruits=fruits)
+
+@app.route("/posts")
+def posts():
+    return 'These are the posts'
