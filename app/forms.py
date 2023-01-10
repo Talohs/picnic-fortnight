9723122e96337182b7e4e7fb94e7a586966a2128
Talohs <blackholesoul@gmail.com>
2023-01-10 16:57:40 -0500

    Added flash messages

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, EmailField
-from wtforms.validators import equal_to, input_required
+from wtforms.validators import EqualTo, InputRequired
 
 class Signupform(FlaskForm):
-    email = EmailField('Email', validators=[input_required()])
-    username = StringField('Username', validators=[input_required()])
-    password = PasswordField('Password', validators=[input_required()])
-    confirm = PasswordField('Confirm Password', validators=[input_required(), equal_to('password')])
+    email = EmailField('Email', validators=[InputRequired()])
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    confirm = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
     submit = SubmitField()
 
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from app.forms import Signupform
 
 @app.route("/")
@@ -11,11 +11,28 @@ def index():
 def posts():
     return render_template('posts.html', name='Dan')
 
-@app.route('/sign-up')
+@app.route('/sign-up',  methods=['GET', 'POST'])
 def signup():
     form = Signupform()
+    if form.validate_on_submit():
+        print('Form submitted and validated')
+        email = form.email.data
+        username = form.username.data
+        password = form.password.data
+        print(email, username, password)
+        flash('Thank you for signing up', 'success')
+        #After sign-up redirect to home
+        
+        # TODO: Check to see if there is a User with that username
+        if username == 'Taloh':
+            flash('That user already exists', 'danger')
+            return redirect(url_for('signup'))
+        # TODO: Create a new user with form data and add to database
+
+        # redirect back to home
+        return redirect(url_for('index'))
     return render_template('sign-up.html', form=form)
 
-@app.route('/log-in')
+@app.route('/log-in',)
 def login():
     return render_template('log-in.html', name='Dan')
